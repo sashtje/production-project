26933ef2fb0f64825ff2bb8c0427b9e54d21b1bd
sashtje <rogova@mindbox.cloud>
2023-06-12 16:36:21 +0300

    feat: add router and code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import {Counter} from "./components/Counter";
+import {Suspense} from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+
 import './index.scss';
 
 export const App = () => {
     return (
         <div className="app">
-            Counter
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
