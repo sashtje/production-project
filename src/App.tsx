5fff20b490450bf354e727d2e135e8aa4fad870f
sashtje <rogova@mindbox.cloud>
2023-06-12 19:07:26 +0300

    feat: add styles and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import {Suspense} from 'react';
+import {Suspense, useContext} from 'react';
 import {Routes, Route, Link} from "react-router-dom";
 
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
 
-import './index.scss';
+import './styles/index.scss';
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>{theme === Theme.DARK ? 'Light' : 'Dark'}</button>
+
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
 
