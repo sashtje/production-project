fde65028d549fcba6fc7fab4beb516f56dc65408
sashtje <sashtje.lefevre@gmail.com>
2023-07-18 18:30:44 +0300

    feat: add NotFoundPage and Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,9 +2,10 @@ import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { routerConfig } from 'shared/config/routerConfig';
+import { PageLoader } from 'shared/ui/PageLoader';
 
 export const AppRouter = () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<PageLoader />}>
     <Routes>
       {Object.values(routerConfig).map(({ path, element }) => (
         <Route
---
+++ src/shared/ui/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,15 @@
+import { classNames } from 'shared/lib/classNames';
+
+import { Loader } from 'shared/ui/Loader';
+
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+    className?: string;
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => (
+  <div className={classNames(cls.pageLoader, {}, [className])}>
+    <Loader />
+  </div>
+);
