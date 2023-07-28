1afc6e97a9c3de6ce7042638bc512be56f62de67
sashtje <sashtje.lefevre@gmail.com>
2023-07-28 14:39:48 +0300

    fix: fix Sidebar tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 
 import { Sidebar } from 'widgets';
-import { renderWithTranslation } from 'shared/lib/tests';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
   test('renders sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
 
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('toggles sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
 
     const toggleBtn = screen.getByTestId('sidebar-toggle');
 
