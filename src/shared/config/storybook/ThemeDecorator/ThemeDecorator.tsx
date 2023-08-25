7efd6d9d22ab25fbd23faa2b33045d70c145de2c
sashtje <sashtje.lefevre@gmail.com>
2023-08-25 15:58:53 +0300

    fix: fix ThemeDecorator and fix screenshots because of fixed color

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -2,10 +2,14 @@ import { DecoratorFn } from '@storybook/react';
 
 import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 
-export const ThemeDecorator = (theme: Theme): DecoratorFn => (StoryComponent) => (
-  <ThemeProvider initialTheme={theme}>
-    <div className={`app ${theme}`}>
-      <StoryComponent />
-    </div>
-  </ThemeProvider>
-);
+export const ThemeDecorator = (theme: Theme): DecoratorFn => (StoryComponent) => {
+  document.body.className = theme;
+
+  return (
+    <ThemeProvider initialTheme={theme}>
+      <div className="app">
+        <StoryComponent />
+      </div>
+    </ThemeProvider>
+  );
+};
