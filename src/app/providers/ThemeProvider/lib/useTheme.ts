80e9287133646ff7c96c0cd074399c55d78b18af
sashtje <sashtje.lefevre@gmail.com>
2023-08-31 19:37:33 +0300

    feat: add orange theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -14,8 +14,24 @@ export function useTheme(): UseThemeResult {
   }, [theme]);
 
   const toggleTheme = () => {
-    const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
+    let newTheme: Theme;
+
+    switch (theme) {
+    case Theme.LIGHT:
+      newTheme = Theme.ORANGE;
+      break;
+    case Theme.DARK:
+      newTheme = Theme.LIGHT;
+      break;
+    case Theme.ORANGE:
+      newTheme = Theme.DARK;
+      break;
+    default:
+      newTheme = Theme.LIGHT;
+    }
+
     setTheme?.(newTheme);
+
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
