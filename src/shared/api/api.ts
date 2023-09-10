c890a77602d0a5bd8b5949cb9ae4d95a098784fd
sashtje <sashtje.lefevre@gmail.com>
2023-09-10 20:04:19 +0300

    fix: fix api config

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -4,7 +4,13 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
-  },
+});
+
+// перехватчик запроса, отрабатывает перед каждым запросом
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || '';
+  }
+
+  return config;
 });
