d5455164c9f362a0fce031e87d165559bd3a44c1
sashtje <sashtje.lefevre@gmail.com>
2023-10-13 23:49:42 +0300

    fix: fix error with removed styles

--- src/widgets/ArticalAdditionalInfo/ui/ArticalAdditionalInfo.tsx
+++ src/widgets/ArticalAdditionalInfo/ui/ArticalAdditionalInfo.tsx
@@ -2,15 +2,12 @@ import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
-import { classNames } from '@/shared/lib/classNames';
 import { User } from '@/entities/User';
 import { HStack, VStack } from '@/shared/ui/redesigned/Stack';
 import { Avatar } from '@/shared/ui/redesigned/Avatar';
 import { Text } from '@/shared/ui/redesigned/Text';
 import { Button } from '@/shared/ui/redesigned/Button';
 import { getCanEditArticle } from '@/pages/ArticleDetailsPage/model/selectors/article';
-
-import cls from './ArticalAdditionalInfo.module.scss';
 
 interface ArticalAdditionalInfoProps {
   className?: string;
@@ -27,7 +24,7 @@ export const ArticalAdditionalInfo = memo((props: ArticalAdditionalInfoProps) =>
   const { t } = useTranslation('articles');
 
   return (
-    <VStack gap="32" className={classNames(cls.articalAdditionalInfo, {}, [className])}>
+    <VStack gap="32" className={className}>
       <HStack gap="8">
         <Avatar src={author.avatar} size={32} />
 
